59d7d1ca3239c50f03570c911296ce126f076430
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-07 20:15:37 +0530

    Solution to Check Leap Year

--- Check Leap Year@@6788dd63490637cfc44e1b90/code.c
+++ Check Leap Year@@6788dd63490637cfc44e1b90/code.c
@@ -1,6 +1,7 @@
 // Your code here...
 // Your code here...
 #include <stdio.h>
+#include <stdbool.h>
 
 #define ERROR 1
 #define SUCCESS 0
@@ -12,7 +13,8 @@ int main(int argc, char *const av [])
         printf("Error");
         return ERROR;
     }
-    bool res = (((a % 4) == 0) && ((a % 400) == 0) && ((a % 100) == 0))
+    bool res = (((a % 4) == 0) && ((a % 400) == 0) && ((a % 100) == 0));
+
     if ((printf("%s\n", (res) ? "Leap Year" : "Not a Leap Year")) < 0)
         return ERROR;
     return SUCCESS;
