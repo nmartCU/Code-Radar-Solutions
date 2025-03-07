b84e55b1f45231d89d7dcff0a7ffeddd957406df
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-07 18:40:08 +0530

    Solution to Input and Print Two Strings

---
+++ Input and Print Two Strings@@6788d5012db25efec946f940/code.c
@@ -0,0 +1,19 @@
+// Your code here...
+
+// Your code here...
+#include <stdio.h>
+
+#define ERROR 1
+#define SUCCESS 0
+#define MAX_LENGTH 256
+
+int main(int argc, char *const av [])
+{
+    char str[MAX_LENGTH], str2[MAX_LENGTH] = {0};
+
+    if ((scanf("%s %s \n", str, str2)) != 2)
+        return ERROR;
+    if ((printf("You entered: %s and %s\n", str, str2)) < 0)
+        return ERROR;
+    return SUCCESS;
+}
