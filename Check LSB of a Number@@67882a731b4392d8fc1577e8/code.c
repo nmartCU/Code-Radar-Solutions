fe165816b5407318fdf1bcd63c369a1cbc524ba2
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-07 19:35:40 +0530

    Solution to Check LSB of a Number

--- Check LSB of a Number@@67882a731b4392d8fc1577e8/code.c
+++ Check LSB of a Number@@67882a731b4392d8fc1577e8/code.c
@@ -7,10 +7,8 @@
 int main(int argc, char *const av [])
 {
     int a = 0;
-    if ((scanf("%d ", &a)) != 1) {
-        printf("Error");
+    if ((scanf("%d ", &a)) != 1)
         return ERROR;
-    }
     // (a % 2) => CUZ if LSB == 1 then Odds AND if LSB == 0 then Even
     if ((printf("%s\n", (a & 1) ? "Set" : "Not Set")) < 0)
         return ERROR;
