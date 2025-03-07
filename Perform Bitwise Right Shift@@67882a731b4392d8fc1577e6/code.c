5441d1262cea3f20624a69b112677f5977f38695
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-07 19:25:19 +0530

    Solution to Perform Bitwise Right Shift

---
+++ Perform Bitwise Right Shift@@67882a731b4392d8fc1577e6/code.c
@@ -0,0 +1,18 @@
+// Your code here...
+// Your code here...
+#include <stdio.h>
+
+#define ERROR 1
+#define SUCCESS 0
+
+int main(int argc, char *const av [])
+{
+    int a, b = 0;
+    if ((scanf("%d %d", &a, &b)) != 2) {
+        printf("Error");
+        return ERROR;
+    }
+    if ((printf("%d\n", (a >> b))) < 0)
+        return ERROR;
+    return SUCCESS;
+}
