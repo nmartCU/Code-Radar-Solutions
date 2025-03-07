f5ac59dd8236743139a8f1dee6a8a5574a83fd7c
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-07 19:23:31 +0530

    Solution to Perform Bitwise AND

---
+++ Perform Bitwise AND@@67882a731b4392d8fc1577e3/code.c
@@ -0,0 +1,17 @@
+// Your code here...
+#include <stdio.h>
+
+#define ERROR 1
+#define SUCCESS 0
+
+int main(int argc, char *const av [])
+{
+    int a, b = 0;
+    if ((scanf("%d %d", &a, &b)) != 2) {
+        printf("Error");
+        return ERROR;
+    }
+    if ((printf("%d\n", (a & b))) < 0)
+        return ERROR;
+    return SUCCESS;
+}
