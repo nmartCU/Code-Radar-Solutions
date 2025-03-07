b892b3bbd34c96cf020092e843eec3a83c798094
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-07 21:49:58 +0530

    Solution to Check Even or Odd Using Division

--- Check Even or Odd Using Division@@6788dd63490637cfc44e1b9b/code.c
+++ Check Even or Odd Using Division@@6788dd63490637cfc44e1b9b/code.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 #define ERROR 1
 #define SUCCESS 0
-
-bool isDivisble(int a)
-{
-    int res = 0;
-    while (a != 0) {
-        res /= 2;
-        a = res;
-    }
-    if (res == 0) {
-        return true;
-    }
-    return false;
-}
 
 int main(int argc, char *const av [])
 {
     int a = 0;
     if ((scanf("%d", &a)) != 1)
         return ERROR;
-    if ((printf("%s\n", (isDivisble(a)) ? "Even" : "Odd")) < 0)
+    if ((printf("%s\n", (((a / 2) * 2) == a) ? "Even" : "Odd")) < 0)
         return ERROR;
     return SUCCESS;
 }
