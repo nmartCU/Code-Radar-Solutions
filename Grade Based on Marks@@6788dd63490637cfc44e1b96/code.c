4ddf773bda614ebd34c9ffedb75605fd2b66a911
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-07 21:20:29 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -13,8 +13,8 @@ int main(int argc, char *const argv[])
     
     if ((scanf("%d", &a)) != 1)
         return ERROR;
-    
-    if ((printf("%s\n", (a >= 90) ? "A" : (a >= 80 && a < 90) ? "B" : (a >= 70 && a < 80) ? "C" : (a >= 60 && a < 70) ? 'D' : 'F')) < 0)
+    char grade = (a >= 90) ? "A" : (a >= 80 && a < 90) ? "B" : (a >= 70 && a < 80) ? "C" : (a >= 60 && a < 70) ? 'D' : 'F';
+    if ((printf("%c\n", grade)) < 0)
         return ERROR;
     return SUCCESS;
 }
