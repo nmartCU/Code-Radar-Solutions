e27de1444c7d57be71b7a93e0efc9c9d5fb4bc9f
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-08 00:29:41 +0530

    Solution to Sum of First N Natural Numbers

--- Sum of First N Natural Numbers@@676c4885c18c9c17a0f50667/code.c
+++ Sum of First N Natural Numbers@@676c4885c18c9c17a0f50667/code.c
@@ -9,7 +9,7 @@ int main(int argc, char *const argv[])
     int a = 0;
     if ((scanf("%d ", &a)) != 1)
         return ERROR;
-    if ((printf("%d ", (a * (a + 1) / 2)) < 0)
+    if ((printf("%d ", (a * (a + 1) / 2))) < 0)
         return ERROR;
     return SUCCESS;
 }
