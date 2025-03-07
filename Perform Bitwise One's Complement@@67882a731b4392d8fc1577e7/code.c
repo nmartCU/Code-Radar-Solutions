65c624bb24c6be4ca05caaebfe0220bc462eb39b
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-07 19:26:42 +0530

    Solution to Perform Bitwise One's Complement

---
+++ Perform Bitwise One's Complement@@67882a731b4392d8fc1577e7/code.c
@@ -0,0 +1,17 @@
+// Your code here...
+#include <stdio.h>
+
+#define ERROR 1
+#define SUCCESS 0
+
+int main(int argc, char *const av [])
+{
+    int a = 0;
+    if ((scanf("%d \n", &a)) != 1) {
+        printf("Error");
+        return ERROR;
+    }
+    if ((printf("%d\n", (~a))) < 0)
+        return ERROR;
+    return SUCCESS;
+}
