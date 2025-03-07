fef053c64d50ae9cd4a6e8f82b6e9567e3905743
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-07 18:16:24 +0530

    Solution to Input and Print a String

--- Input and Print a String@@6788d5012db25efec946f93c/code.c
+++ Input and Print a String@@6788d5012db25efec946f93c/code.c
@@ -3,11 +3,11 @@
 
 #define ERROR 1
 #define SUCCESS 0
-
+#define MAX_LENGTH 256
 
 int main(int main, char *const av[])
 {
-    char str[];
+    char str[MAX_LENGTH] = [0];
 
     if ((scanf("%s", &str)) != 1)
         return ERROR;
