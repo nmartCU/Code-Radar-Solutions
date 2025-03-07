c62e60e61924360ecb4d7c8cd0726e48481ae2e7
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-07 19:17:39 +0530

    Solution to Check Opposite Signs

---
+++ Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
@@ -0,0 +1,15 @@
+// Your code here...
+#include <stdio.h>
+
+#define ERROR 1
+#define SUCCESS 0
+
+int main(int argc, char *const av [])
+{
+    int a, b = 0;
+    if ((scanf("%d %d \n", &a, &b)) != 2)
+        return ERROR;
+    if ((printf("%s\n", (((a < 0 && b > 0) || (a > 0 && b < 0)) ? "True" : "False"))) < 0)
+        return ERROR;
+    return SUCCESS;
+}
