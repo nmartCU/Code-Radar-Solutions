a3e2cd9b5f33b5c11e81814145fd7dd8bd2db42e
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-08 00:33:36 +0530

    Solution to Print Multiplication Table

---
+++ Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
@@ -0,0 +1,21 @@
+// Your code here...
+// Your code here...
+#include <stdio.h>
+
+#define ERROR 1
+#define SUCCESS 0
+
+int main(int argc, char *const argv[])
+{
+    int a = 0;
+
+    if ((scanf("%d ", &a)) != 1)
+        return ERROR;
+    int sum = 0;
+    for (int i = 0; i < a; i++) {
+        sum *= (i + 1);
+        if ((printf("%d x %d = %sum", a, i + 1, sum)) < 0)
+            return ERROR;
+    }
+    return SUCCESS;
+}
