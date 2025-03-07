646a0cf0842a0d80f361d85a057123c79e257c59
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-07 21:05:28 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,20 @@
+// Your code here...
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+#define ERROR 1
+#define SUCCESS 0
+
+int main(int argc, char *const argv[])
+{
+    char c = 0;
+    if ((scanf("%d %d %d \n", &a, &b, &c)) != 3)
+        return ERROR;
+    char lower = tolower(c);
+    bool isVowel = (isalpha(lower) && (lower_ch == 'a' || lower_ch == 'e' || lower_ch == 'i' || lower_ch == 'o' || lower_ch == 'u' || lower_ch == 'y' ));
+    bool isConsonant = (isalpha(lower) && !isVowel);
+    if ((printf("%s\n", (isVowel ? "Vowel" : isConsonant ? "Consonant" : isdigit(c) ? "Digit" : "Special Character"))) < 0)
+        return ERROR;
+    return SUCCESS;
+}
