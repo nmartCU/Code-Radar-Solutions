d9e01ab45170750a8b953fce1000c36f2427bd72
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-07 18:41:53 +0530

    Solution to Input and Print a Double

---
+++ Input and Print a Double@@6788d5012db25efec946f942/code.c
@@ -0,0 +1,15 @@
+// Your code here...
+#include <stdio.h>
+
+#define ERROR 1
+#define SUCCESS 0
+
+int main(int argc, char *const av [])
+{
+    float a = 0;
+    if ((scanf("%f \n", &a)) != 1)
+        return ERROR;
+    if ((printf("You entered: %0.4f\n", a)) < 0)
+        return ERROR;
+    return SUCCESS;
+}
