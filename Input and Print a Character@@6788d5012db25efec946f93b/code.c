a78096e75e6c526bbd8ed189a26b5ca18efd17cb
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-07 18:01:38 +0530

    Solution to Input and Print a Character

--- Input and Print a Character@@6788d5012db25efec946f93b/code.c
+++ Input and Print a Character@@6788d5012db25efec946f93b/code.c
@@ -1,13 +1,13 @@
 // Your code here...
 #include <stdio.h>
 
-#DEFINE ERROR 1
-#DEFINE SUCCESS 0
+#DEFINE ERROR (1)
+#DEFINE SUCCESS (0)
 
 int main()
 {
     char c = 0;
-    if (scanf("%c", &c) != 1) {
+    if (()scanf("%c", &c)) != 1) {
         printf("Error scanf");
         return ERROR;
     }
