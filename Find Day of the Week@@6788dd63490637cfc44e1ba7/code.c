c21c40d4548c139b02e0e87261661506d82eda34
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-07 22:38:08 +0530

    Solution to Find Day of the Week

---
+++ Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
@@ -0,0 +1,18 @@
+// Your code here...
+#include <stdio.h>
+#include <stdbool.h>
+
+#define ERROR 1
+#define SUCCESS 0
+
+const dayOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+int main(int argc, char *const av [])
+{
+    int a = 0;
+    if ((scanf("%d", &a)) != 1)
+        return ERROR;
+    if ((printf("%s\n", (a >= 1 && a <= 7 ? dayOfWeek[a - 1] : "Invalid"))) < 0)
+        return ERROR;
+    return SUCCESS;
+}
