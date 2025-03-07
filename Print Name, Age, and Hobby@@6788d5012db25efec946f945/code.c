aa28e4349676ed7a7873f25bee2aae36ee12713b
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-07 18:49:55 +0530

    Solution to Print Name, Age, and Hobby

---
+++ Print Name, Age, and Hobby@@6788d5012db25efec946f945/code.c
@@ -0,0 +1,19 @@
+// Your code here...
+// Your code here...
+// Your code here...
+#include <stdio.h>
+
+#define ERROR 1
+#define SUCCESS 0
+#define MAX_LENGTH 256
+
+int main(int argc, char *const av [])
+{
+    char name[MAX_LENGTH], hobby[MAX_LENGTH] = {0};
+    int age = 0;
+    if ((scanf("%s %d %s \n", name, age, hobby)) != 3)
+        return ERROR;
+    if ((printf("Name: %s\nAge: %d\nHobby: %s", name, age, hobby)) < 0)
+        return ERROR;
+    return SUCCESS;
+}
