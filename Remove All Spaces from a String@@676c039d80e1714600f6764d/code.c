1c17ea8122a1252deb577227128aefc868fb054b
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-08 19:59:07 +0530

    Solution to Remove All Spaces from a String

---
+++ Remove All Spaces from a String@@676c039d80e1714600f6764d/code.c
@@ -0,0 +1,44 @@
+// Your code here...
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define ERROR 1
+#define SUCCESS 0
+#define MAX_LENGTH 256
+
+
+int main(int argc, char *const argv[])
+{
+    char *str = NULL;
+    char *res = NULL;
+    size_t len = 0;
+    ssize_t read = 0;
+    int size = 0;
+    int lenghtWord = 0;
+
+    if ((read = getline(&str, &len, stdin)) == -1) {
+        free(str);
+        return ERROR;
+    }
+    if (str[read - 1] == '\n') {
+        str[read - 1] = '\0';
+        --read;
+    }
+    if ((res = malloc(sizeof(char) * (read + 1))) == NULL) {
+        free(str);
+        return ERROR;
+    }
+    for (int i = 0; i <= read; i++) {
+        if (str[i] == ' ' || str[i] == '\t')
+            continue;
+        res[i] = str[i];
+    }
+    if ((printf("%s\n", str)) < 0) {
+        free(str);
+        return ERROR;
+    }
+    free(str);
+    return SUCCESS;
+}
