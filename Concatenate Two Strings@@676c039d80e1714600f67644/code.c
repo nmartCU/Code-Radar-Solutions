88db9832aaf472719f8050a39099d8942b8bca93
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-08 16:04:43 +0530

    Solution to Concatenate Two Strings

---
+++ Concatenate Two Strings@@676c039d80e1714600f67644/code.c
@@ -0,0 +1,48 @@
+// Your code here...
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define ERROR 1
+#define SUCCESS 0
+#define MAX_LENGTH 256
+
+int main(int argc, char *const argv[])
+{
+    char *str[2] = {NULL, NULL};
+    size_t len[2] = {0, 0};
+    ssize_t read[2] = {0, 0};
+    int size = 0;
+        
+    for (int i = 0; i < 2; i++) {
+        if ((read[i] = getline(&str[i], &len[i], stdin)) == -1) {
+            for (int j = 0; j < i; j++) {
+                free(str[j]);
+            }
+            return ERROR;
+        }
+        if (str[i] && read[i] > 0 && str[i][read[i] - 1] == '\n') {
+            str[i][read[i] - 1] = '\0';
+        }
+    }
+    
+    for (int i = 0; str[0][i] != '\0'; i++) {
+        if (printf("%c\n", str[0][i]) < 0) {
+            free(str[0]);
+            free(str[1]);
+        }
+        return ERROR;
+    }
+    for (int i = 0; str[1][i] != '\0'; i++) {
+        if (printf("%c\n", str[1][i]) < 0) {
+            free(str[0]);
+            free(str[1]);
+        }
+        return ERROR;
+    }
+    free(str[0])
+    free(str[1]);
+    return SUCCESS;
+}
