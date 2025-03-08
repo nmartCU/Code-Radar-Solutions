6921ea8351747069be2a5fec7a9f2c571233085a
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-08 15:36:36 +0530

    Solution to Find the Length of a String

---
+++ Find the Length of a String@@676c039d80e1714600f67643/code.c
@@ -0,0 +1,33 @@
+// Your code here...
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define ERROR 1
+#define SUCCESS 0
+#define MAX_LENGTH 256
+
+int main(int argc, char *const argv[])
+{
+    char *str = NULL;
+    size_t len = 0;
+    ssize_t read = 0;
+    int size = 0;
+        
+    if ((read = getline(&str, &len, stdin)) == -1) {
+        free(str);
+        return ERROR;
+    };
+    
+    for (int i = 0; str[i] != '\0'; i++) {
+        ++size;
+    }
+    if (printf("%d\n", size) < 0) {
+        free(str);
+        return ERROR;
+    }    
+    free(str);
+    return SUCCESS;
+}
