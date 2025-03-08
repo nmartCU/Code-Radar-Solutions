97a7643254bb6be3d09250bf4a0da07c8823dc3e
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-08 18:20:05 +0530

    Solution to Replace a Character in a String

--- Replace a Character in a String@@676c039d80e1714600f67648/code.c
+++ Replace a Character in a String@@676c039d80e1714600f67648/code.c
@@ -11,7 +11,7 @@
 
 void my_free(char **str)
 {
-    for (int i = 0; i < 3 ; i++)
+    for (int i = 0; str[i]; i++)
         free(str[i]);
 }
 
@@ -40,8 +40,8 @@ int main(int argc, char *const argv[])
     result[read[0]] = '\0';
     for (int i = 0; i < read[0]; i++) {
         char current = str[0][i];
-        if (current == str[1][0]) {
-            result[i] = str[2][0];
+        if (i == read[1] && i == read[2] && current == str[1][i]) {
+            result[i] = str[2][i];
         } else {
             result[i] = current;
         }
