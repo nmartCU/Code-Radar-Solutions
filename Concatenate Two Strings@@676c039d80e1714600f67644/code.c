5350312b8a70812dad28f507593809d2f3fb9bea
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-08 16:28:04 +0530

    Solution to Concatenate Two Strings

--- Concatenate Two Strings@@676c039d80e1714600f67644/code.c
+++ Concatenate Two Strings@@676c039d80e1714600f67644/code.c
@@ -9,6 +9,12 @@
 #define SUCCESS 0
 #define MAX_LENGTH 256
 
+void my_free(char *str, char *str2)
+{
+    free(str);
+    free(str2);
+}
+
 int main(int argc, char *const argv[])
 {
     char *str[2] = {NULL, NULL};
@@ -19,9 +25,8 @@ int main(int argc, char *const argv[])
         
     for (int i = 0; i < 2; i++) {
         if ((read[i] = getline(&str[i], &len[i], stdin)) == -1) {
-            for (int j = 0; j < i; j++) {
+            for (int j = 0; j < i; j++)
                 free(str[j]);
-            }
             return ERROR;
         }
         if (str[i] && read[i] > 0 && str[i][read[i] - 1] == '\n') {
@@ -31,8 +36,7 @@ int main(int argc, char *const argv[])
     }
     size = read[0] + read[1];
     if ((result = malloc(sizeof(char) * (size + 1))) == NULL) {
-        free(str[0]);
-        free(str[1]);
+        my_free(str[0], str[1]);
         return ERROR;
     }
     result[size] = '\0';
@@ -42,13 +46,11 @@ int main(int argc, char *const argv[])
         result[j + read[0]] = str[1][j];
     }
     if (printf("%s", result) < 0) {
-        free(str[0]);
-        free(str[1]);
+        my_free(str[0], str[1]);
         free(result);
         return ERROR;
     }
-    free(str[0]);
-    free(str[1]);
+    my_free(str[0], str[1]);
     free(result);
     return SUCCESS;
 }
