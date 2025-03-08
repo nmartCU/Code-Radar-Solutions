49374eb7c2f64c70bfa4d44037b82398b3bc6a05
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-08 14:19:20 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -6,6 +6,20 @@
 #define ERROR 1
 #define SUCCESS 0
 
+static int print(int a, int i)
+{
+    for (int j = 1; j <= a - i; j++)
+        if ((printf(" ")) < 0)
+            return ERROR;      
+    for (int j = 1; j <= (2 * i - 1); j++) {
+        if ((printf("*")) < 0)
+            return ERROR;    
+    }
+    if ((printf("\n")) < 0)
+        return ERROR;
+    return SUCCESS;
+}
+
 int main(int argc, char *const argv[])
 {
     int a = 0;
@@ -13,25 +27,11 @@ int main(int argc, char *const argv[])
     if ((scanf("%d ", &a)) != 1)
         return ERROR;
     for (int i = 1; i <= a; i++) {
-        for (int j = 1; j <= a - i; j++)
-            if ((printf(" ")) < 0)
-                return ERROR;      
-        for (int j = 1; j <= (2 * i - 1); j++) {
-            if ((printf("*")) < 0)
-                return ERROR;    
-        }
-        if ((printf("\n")) < 0)
+        if (print(a, i) == ERROR)
             return ERROR;
     }
     for (int i = a - 1; i >= 1; i--) {
-        for (int j = 1; j <= a - i; j++)
-            if ((printf(" ")) < 0)
-                return ERROR;      
-        for (int j = 1; j <= (2 * i - 1); j++) {
-            if ((printf("*")) < 0)
-                return ERROR;    
-        }
-        if ((printf("\n")) < 0)
-            return ERROR;
+        if (print(a, i) == ERROR)
+            return ERROR; 
     }
 }
