a1a6637f928bdd6b5ed9a22bd9a4faee6e0ab12b
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-11 01:04:28 +0530

    Solution to Create a Program to Store and Display Student Records

--- Create a Program to Store and Display Student Records@@6785070ed37d6039a711ebee/code.c
+++ Create a Program to Store and Display Student Records@@6785070ed37d6039a711ebee/code.c
@@ -6,6 +6,7 @@
 
 #define ERROR 1
 #define SUCCESS 0
+#define MAX_LENGTH 256
 
 typedef struct student_s {
     int roll;
@@ -23,7 +24,18 @@ int main(int argc, char *const argv[])
     if ((students = malloc(sizeof(student_t) * n)) == NULL)
         return ERROR;
     for (int i = 0; i < n; i++) {
-        if ((scanf("%d %s %f", &students[i].roll, students[i].name, &students[i].marks)) != 3) {
+        char temp[MAX_LENGTH] = {0};
+        if ((scanf("%d %s %f", &students[i].roll, temp, &students[i].marks)) != 3) {
+            for (int j = 0; j < i; j++) {
+                free(students[j].name);
+            }
+            free(students);
+            return ERROR;
+        }
+        if ((students[i].name = malloc(sizeof(char) * (strlen(temp) + 1))) == NULL && strcpy(students[i].name, temp) == NULL) {
+            for (int j = 0; j < i; j++) {
+                free(students[j].name);
+            }
             free(students);
             return ERROR;
         }
@@ -32,6 +44,8 @@ int main(int argc, char *const argv[])
         if ((printf("Roll Number: %d, Name: %s, Marks: %f", students[i].roll, students[i].name, students[i].marks)) < 0) {
             return ERROR;
         }
+        free(students[i].name);
     }
+    free(students);
     return SUCCESS;
 }
