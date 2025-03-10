6a223dc48f6cfaa073492730fb26c793d6d0fc13
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-11 01:21:09 +0530

    Solution to Find and Display the Top Scorer

---
+++ Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
@@ -0,0 +1,58 @@
+// Your code here...
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define ERROR 1
+#define SUCCESS 0
+#define MAX_LENGTH 256
+
+typedef struct student_s {
+    int roll;
+    char *name;
+    float marks;
+} student_t;
+
+int main(int argc, char *const argv[])
+{
+    int n = 0;
+    student_t *students = NULL;
+    int max = 0;
+        
+    if ((scanf("%d", &n)) != 1)
+        return ERROR;
+    if ((students = malloc(sizeof(student_t) * (n))) == NULL)
+        return ERROR;
+    for (int i = 0; i < n; i++) {
+        char temp[MAX_LENGTH] = {0};
+        if ((scanf("%d %s %f", &students[i].roll, temp, &students[i].marks)) != 3) {
+            for (int j = 0; j < i; j++)
+                free(students[j].name);
+            free(students);
+            return ERROR;
+        }
+        if ((students[i].name = malloc(sizeof(char) * (strlen(temp) + 1))) == NULL) {
+            for (int j = 0; j < i; j++) {
+                free(students[j].name);
+            }
+            free(students);
+            return ERROR;
+        }
+        strcpy(students[i].name, temp);
+        if (max < student[i].marks) {
+            max = i;
+        }
+    }
+    
+    if ((printf("Top Scorer: Roll Number: %d, Name: %s, Marks: %0.2f", students[max].roll, students[max].name, students[max].marks)) < 0) {
+        for (int i = 0; i < n; i++)
+            free(students[i].name)
+        free(students);
+        return ERROR;
+    }
+    for (int i = 0; i < n; i++)
+        free(students[i].name)
+    free(students);
+    return SUCCESS;
+}
