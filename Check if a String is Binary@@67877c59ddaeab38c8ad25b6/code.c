be6996ea53812b9c507a280a1ccea364c8e87585
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-10 23:50:33 +0530

    Solution to Check if a String is Binary

---
+++ Check if a String is Binary@@67877c59ddaeab38c8ad25b6/code.c
@@ -0,0 +1,40 @@
+// Your code here...
+// Your code here...
+// Your code here...
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#define ERROR 1
+#define SUCCESS 0
+#define MAX_LENGTH 256
+
+int main(int argc, char *const argv[])
+{
+    char *str = NULL;
+    size_t len = 0;
+    ssize_t read = 0;
+    bool notbin = false;
+        
+    if ((read = getline(&str, &len, stdin)) == -1) {
+        free(str);
+        return ERROR;
+    };
+    if (read > 0 && str && str[read - 1] == '\n') {
+        str[read - 1] = '\0';
+        --read;
+    }
+    for (int i = 0; i < read; i++) {
+        if (str[i] != '0' || str[i] != '1') {
+            notbin = true;
+            break;
+        }
+    }
+    free(str);
+    if ((printf("%s\n", notbin ? "No": "Yes")) < 0)
+        return ERROR;
+    return SUCCESS;
+}
