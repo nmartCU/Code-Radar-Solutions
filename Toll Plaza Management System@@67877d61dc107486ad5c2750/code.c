c25a719e764f106cfc8bf92d8512c1e0b8609163
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-11 03:29:18 +0530

    Solution to Toll Plaza Management System

--- Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -60,6 +60,8 @@ int main(int argc, char *const argv[])
         } else {
             bike += students[i].price;
         }
+        printf("Name is : %s size: %d\n", students[i].name, strlen(students[i].name));
+        printf("Name is %s\n car: %d truck: %d bike %d", students[i].name, car, truck, bike)
     }
 
     my_free(students, n);
