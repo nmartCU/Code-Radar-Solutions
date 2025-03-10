95ab806b3cdd472fc30c73590c399127f245ced0
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-11 01:29:15 +0530

    Solution to Calculate Average Marks of Students

---
+++ Calculate Average Marks of Students@@67877d61dc107486ad5c2743/code.c
@@ -0,0 +1,57 @@
+// Your code here...
+// Your code here...
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define ERROR 1
+#define SUCCESS 0
+#define MAX_LENGTH 256
+
+typedef struct student_s {
+    int roll;
+    char *name;
+    float marks;
+} student_t;
+
+int main(int argc, char *const argv[])
+{
+    int n = 0;
+    student_t *students = NULL;
+    float average = 0f;
+        
+    if ((scanf("%d", &n)) != 1)
+        return ERROR;
+    if ((students = malloc(sizeof(student_t) * (n))) == NULL)
+        return ERROR;
+    for (int i = 0; i < n; i++) {
+        char temp[MAX_LENGTH] = {0};
+        if ((scanf("%d %s %f", &students[i].roll, temp, &students[i].marks)) != 3) {
+            for (int j = 0; j < i; j++)
+                free(students[j].name);
+            free(students);
+            return ERROR;
+        }
+        if ((students[i].name = malloc(sizeof(char) * (strlen(temp) + 1))) == NULL) {
+            for (int j = 0; j < i; j++) {
+                free(students[j].name);
+            }
+            free(students);
+            return ERROR;
+        }
+        strcpy(students[i].name, temp);
+        average += students[i].marks;
+    }
+
+
+    if ((printf("Average Marks: %0.2f\n", average / n)) < 0) {
+        for (int j = 0; i < n; j++)
+            free(students[i].name);
+        free(students);
+        return ERROR;
+    }
+    free(students[i].name);
+    free(students);
+    return SUCCESS;
+}
