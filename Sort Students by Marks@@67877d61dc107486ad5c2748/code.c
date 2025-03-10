0fc039f384531f1bcff50eeb924e9847d3295811
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-11 02:16:50 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -52,7 +52,7 @@ int main(int argc, char *const argv[])
 
     qsort(students, n, sizeof(student_t), compare_students);
     for (int i = 0; i < n; i++) {
-        if ((printf(" Roll Number: %d, Name: %s, Marks: %0.2f\n", students[i].roll, student[i].name, students[i].marks)) < 0) {
+        if ((printf(" Roll Number: %d, Name: %s, Marks: %0.2f\n", students[i].roll, students[i].name, students[i].marks)) < 0) {
             my_free(students, n);
             return ERROR;
         }
