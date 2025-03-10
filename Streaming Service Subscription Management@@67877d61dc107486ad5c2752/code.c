587dc88d34a22acfe3236651c2017e361d157efb
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-11 03:49:35 +0530

    Solution to Streaming Service Subscription Management

--- Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
+++ Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
@@ -30,7 +30,7 @@ int main(int argc, char *const argv[])
 {
     int n = 0;
     sub_t *students = NULL;
-    float basic, premium, standard = 0;
+    float basic, premium, standard = 0.0f;
     int user, user2, user3 = 0;
 
     if ((scanf("%d\n", &n)) != 1)
