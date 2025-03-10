e5ac0c497369a7d6e0e92e3862d016bc514fbdce
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-10 21:34:31 +0530

    Solution to Count Distinct Substrings

---
+++ Count Distinct Substrings@@67877c59ddaeab38c8ad25b0/code.c
@@ -0,0 +1,40 @@
+// Your code here...
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define ERROR 1
+#define SUCCESS 0
+#define MAX_LENGTH 256
+
+int main(int argc, char *const argv[])
+{
+    char *str = NULL;
+    size_t len = 0;
+    ssize_t read = 0;
+    int count = 0;
+        
+    if ((read = getline(&str, &len, stdin)) == -1) {
+        free(str);
+        return ERROR;
+    };
+    if (read > 0 && str && str[read - 1] == '\n') {
+        str[read - 1] = '\0';
+        --read;
+    }
+    for (int i = 0; i < read; i++) {
+        for (int j = 0; j < read; j++) {
+            if (str[i] != str[j])
+                ++count;
+        }
+        ++count;
+    }
+    if ((printf("%.*s", max, &str[start])) < 0) {
+        free(str);
+        return ERROR;
+    }
+    free(str);
+    return SUCCESS;
+}
