c40d2c98ba30c5ec959f8c58a59a18e90216a8ad
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-10 23:30:02 +0530

    Solution to Check if a String is a Palindrome After Removing Spaces

---
+++ Check if a String is a Palindrome After Removing Spaces@@67877c59ddaeab38c8ad25b7/code.c
@@ -0,0 +1,77 @@
+// Your code here...
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define ERROR 1
+#define SUCCESS 0
+#define MAX_LENGTH 256
+
+static int is_delim(char c , char *delim)
+{
+    for (int i = 0; delim[i]; i++)
+        if (c == delim[i])
+            return 1;
+    return 0;
+}
+
+static int count(char *src, char *delim)
+{
+    int nb = 0;
+
+    for (int i = 0; src[i]; i++)
+        if (!is_delim(src[i], delim) && is_delim(src[i + 1], delim))
+            nb++;
+    return nb;
+}
+
+static bool isPalindrome(char *str)
+{
+    int end = strlen(str) - 1;
+
+    for (int i = 0; str[i]; i++) {
+        if (str[i] != str[end])
+            return false;
+        --end;
+    }
+    return true;
+}
+
+int main(int argc, char *const argv[])
+{
+    char *str = NULL;
+    char *temp = NULL;
+    size_t len = 0;
+    ssize_t read = 0;
+    int index = 0;
+        
+    if ((read = getline(&str, &len, stdin)) == -1) {
+        if (printf("%d\n", size) < 0) {
+            free(str);
+            return ERROR;
+        }
+        return ERROR;
+    };
+    if (read > 0 && str && str[read - 1] == '\n') {
+        str[read - 1] = '\0';
+        --read;
+    }
+    int words = count(str, " ");
+
+    if ((temp = malloc(sizeof(char) * (read - words + 1))) == NULL) {
+        free(str);
+        return ERROR;
+    }
+    for (int i = 0; i < read; i++) {
+        if (str[i] != ' ')
+            temp[index] = str[i]; 
+    }
+    free(str);
+    if ((printf("%s\n", isPalindrome(temp) ? "Yes" : "No")) < 0) {
+        free(temp);
+        return ERROR;
+    }
+    return SUCCESS;
+}
