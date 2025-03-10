e64e66b97ee1ea8b0ef5569eecfa30b47825e40c
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-10 22:02:14 +0530

    Solution to Find the Frequency of Each Character

--- Find the Frequency of Each Character@@67877c59ddaeab38c8ad25b1/code.c
+++ Find the Frequency of Each Character@@67877c59ddaeab38c8ad25b1/code.c
@@ -1,5 +1,4 @@
 // Your code here...
-// Your code here...
 
 #include <stdbool.h>
 #include <stdio.h>
@@ -10,12 +9,23 @@
 #define SUCCESS 0
 #define MAX_LENGTH 256
 
+bool isDuplicate(char *str, char c)
+{
+    int count = 0;
+
+    for (int i = 0; str[i]; i++) {
+
+    }
+}
+
 int main(int argc, char *const argv[])
 {
     char *str = NULL;
+    char *temp = NULL;
     size_t len = 0;
     ssize_t read = 0;
     int letters[MAX_LENGTH] = {0};
+    int index = 0;
         
     if ((read = getline(&str, &len, stdin)) == -1) {
         free(str);
@@ -25,14 +35,22 @@ int main(int argc, char *const argv[])
         str[read - 1] = '\0';
         --read;
     }
-    for (int i = 0; str[i]; i++) {
-        ++letters[str[i]];
+    if ((temp = malloc(sizeof(char) * (read + 1))) == NULL) {
+        free(str);
+        return ERROR;
     }
     for (int i = 0; str[i]; i++) {
-            if ((printf("%c: %d\n", str[i], letters[str[i]])) < 0) {
-                free(str);
-                return ERROR;
-            }
+        ++letters[str[i]];
+        if (letters[str[i]] == 1) {
+            temp[index] = str[index];
+            ++index;
+        }
+    }
+    for (int i = 0; temp[i]; i++) {
+        if ((printf("%c: %d\n", temp[i], letters[str[i]])) < 0) {
+            free(str);
+            return ERROR;
+        }
     }
     free(str);
     return SUCCESS;
