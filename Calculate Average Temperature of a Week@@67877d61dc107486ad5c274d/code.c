3c659257119f9b45575feffdedbf706c401c8df5
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-11 02:53:16 +0530

    Solution to Calculate Average Temperature of a Week

---
+++ Calculate Average Temperature of a Week@@67877d61dc107486ad5c274d/code.c
@@ -0,0 +1,26 @@
+// Your code here...
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define ERROR 1
+#define SUCCESS 0
+#define INPUT_LENGTH 7
+
+int main(int argc, char *const argv[])
+{
+    int n = 0;
+    int total = 0;
+        
+    for (int i = 0; i < INPUT_LENGTH; i++) {
+        int value = 0;
+        if ((scanf("%d ", value)) != 1)
+            return ERROR;
+        total += value;
+    }
+
+    if ((printf("Average Temperature: %0.2f", (float) total / INPUT_LENGTH)) < 0)
+        return ERROR;
+    return SUCCESS;
+}
