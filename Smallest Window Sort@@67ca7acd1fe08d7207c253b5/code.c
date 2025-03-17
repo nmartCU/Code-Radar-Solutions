aeef2fcb7b223d47f4cefaf0e41464348eb36a91
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-17 15:24:32 +0530

    Solution to Smallest Window Sort

--- Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -7,12 +7,10 @@ int findUnsortedSubarray(int *arr, int n)
     if (!arr)
         return 0;
     int compt = 0;
-    int temp = 0;
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n - i - 1; j++)
-            if (arr[j] > arr[j + 1])
-                ++compt;
-        break;
+        if (arr[i] > arr[i + 1]) {
+            ++compt;
+        }
     }
     return compt;
 }
