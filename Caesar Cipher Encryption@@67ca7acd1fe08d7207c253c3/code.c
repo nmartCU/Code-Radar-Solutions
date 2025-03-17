7cbbad9754998b0886b803304b7ec7589a0f2399
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-17 19:35:12 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,11 +1,13 @@
 // Your code here...
 
 #include <stdio.h>
+#include <string.h>
+
 #define MAX_LEN 100
 
 void caesarCipher(char message[MAX_LEN], int shift, char encrypted[MAX_LEN])
 {
     for (int i = 0; i < strlen(message) - 1; i++) {
-        encrypted[i] = message + shift; 
+        encrypted[i] = message[i] + shift; 
     }
 }
