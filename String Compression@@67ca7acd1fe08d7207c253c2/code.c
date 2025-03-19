c59eaa01040367fc6cb819de906a81ab9e8073eb
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-19 18:13:47 +0530

    Solution to String Compression

--- String Compression@@67ca7acd1fe08d7207c253c2/code.c
+++ String Compression@@67ca7acd1fe08d7207c253c2/code.c
@@ -6,8 +6,9 @@
 void compressString(char str[MAX_LEN], char compress[MAX_LEN * 2])
 {
     int idx = 0;
+    int i = 0;
 
-    for (int i = 0; str[i] != '\0'; i++) {
+    for (; str[i] != '\0'; i++) {
         char temp = str[i];
         int count = 0;
         for (int j = 0; str[j] != '\0'; j++) {
@@ -22,5 +23,7 @@ void compressString(char str[MAX_LEN], char compress[MAX_LEN * 2])
     }
     if (strlen(compress) > strlen(str)) {
         strcpy(compress, str);
+        return;
     }
+    compress[i] = '\0';
 }
