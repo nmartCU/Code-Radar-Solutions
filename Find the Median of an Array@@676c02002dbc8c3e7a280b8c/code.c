93c579f41c188ec6e198f8c09d572dd56a13dd1b
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-21 20:37:59 +0530

    Solution to Find the Median of an Array

---
+++ Find the Median of an Array@@676c02002dbc8c3e7a280b8c/code.c
@@ -0,0 +1,65 @@
+// Your code here...
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void insertionSort(int *arr, int n)
+{
+    for (int i = 1; i < n; i++) {
+        int temp = arr[i];
+        int idx = i - 1;
+        while (idx >= 0 && temp < arr[idx]) {
+            swap(&arr[idx + 1], &arr[idx]);
+            --idx;
+        }
+        arr[idx + 1] = temp;
+    }
+}
+
+void selectionSort(int *arr, int n)
+{
+    for (int i = 1; i < n - 1; i++) {
+        int idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[idx] > arr[j])
+                idx = j;
+        }
+        if (idx != i)
+            swap(&arr[idx], &arr[i]);
+    }
+}
+
+void bubbleSort(int *arr, int n)
+{
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], arr[j + 1]);
+        }
+    }
+}
+
+int main()
+{
+    int n = 0;
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    insertionSort(arr, n);
+    if (n % 2 == 0) {
+        printf("%d", arr[n / 2 - 1] + arr[n / 2 + 1])
+    } else {
+        printf("%d", n%2)
+    }
+    printf("%d", count);
+    return 0;
+}
