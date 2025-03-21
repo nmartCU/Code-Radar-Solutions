8ba20a2b41e0a98587861a9eaf2d4862edaa8d25
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-21 21:14:28 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -3,6 +3,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct pair_s {
+    int a;
+    int b;
+} pair_t;
+
+bool isNotPairInside(pair_t *pairs, int n, int a, int b)
+(
+    for (int i = 0; i < n; i++) {
+        if ((pairs[i].a == a && pairs[i].b == a) || (pairs[i].b == a && pairs[i].a == a))
+            return false;
+    }
+    return true;
+)
+
 int main()
 {
     int n = 0;
@@ -13,6 +27,7 @@ int main()
 
     int arr[n];
 
+    pair_t *pairs = malloc(sizeof(pair_t) * (n * (n - 1) / 2));
 
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
@@ -23,9 +38,17 @@ int main()
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             if (arr[i] + arr[j] == k) {
-                printf("%d %d\n", arr[i], arr[j]);
+                if (isNotPairInside(pairs, index, arr[i], arr[j])) {
+                    pairs[index].a = arr[i];
+                    pairs[index].b = arr[j];
+                    index++;
+                }
             }
         }
+    }
+
+    for (int i = 0; i < index; i++) {
+        printf("%d %d\n", pairs[i].a, pairs[i].b);
     }
     
     return 0;
