311d591d12bd694e69e56859e25d4a42fc8f0166
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-22 00:02:41 +0530

    Solution to Kth Missing Positive Number

---
+++ Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
@@ -0,0 +1,45 @@
+// Your code here...
+// Your code here...
+
+int maxVal(int arr[], int n)
+{
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return max;
+}
+
+int findKthMissing(int arr[], int n, int k)
+{
+    // 2 3 4 7 11 -- 5
+    // 1 = 1, 5 = 2, 6 = 3, 8 = 4, 9 = 5 => 5 == 5 => 9
+
+    // 2 3 4 7 11
+    // 0 2 3 4 0 0 7 0 0 0 11
+    // k = 5 donc 
+    // loop to temp:
+    //      if (tep[i] == 0) val = i k-- ensuite if (k == 0) return val
+
+    int sizeMax = maxVal(arr, n);
+    int temp[sizeMax];
+
+    for (int i = 0; i < sizeMax; i++) {
+        sizeMax[i] = 0;
+    }
+
+    int val = 0;
+
+    for (int i = 0; i < sizeMax; i++) {
+        if (temp[i] == 0) {
+            val = i;
+            k--;
+        }
+        if (k == 0) {
+            break;
+        }
+    }
+    printf("%d", val);
+    return kth;
+}
