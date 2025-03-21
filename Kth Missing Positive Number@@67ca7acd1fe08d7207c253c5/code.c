1af544f5c59ea251ec3d087a3e786ccf028e465a
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-22 02:43:43 +0530

    Solution to Kth Missing Positive Number

--- Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
+++ Kth Missing Positive Number@@67ca7acd1fe08d7207c253c5/code.c
@@ -1,47 +1,32 @@
 // Your code here...
 
-int maxVal(int arr[], int n)
-{
-    int max = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > max)
-            max = arr[i];
-    }
-    return max;
-}
-
 int findKthMissing(int arr[], int n, int k)
 {
     // 2 3 4 7 11 -- 5
     // 1 = 1, 5 = 2, 6 = 3, 8 = 4, 9 = 5 => 5 == 5 => 9
 
     // 2 3 4 7 11
-    // 0 1 1 1 0 0 1 0 0 0 1
-    // k = 5 donc 
-    // loop to temp:
-    //      if (tep[i] == 0) val = i k-- ensuite if (k == 0) return val
+    // kth = 0
+    // par a temp = 1
+    //    je regarde si temp < arr[i] = 1 < 2
+        // si true kth++ kth = 2 et je regarde si kth == k => stop return
+        // sinon ++i 
 
-    int sizeMax = maxVal(arr, n);
-    int temp[sizeMax];
+    // 
 
-    for (int i = 0; i < sizeMax; i++) {
-        temp[i] = 0;
-    }
+    int temp = 1;
+    int kth = 0;
+    int index = 0;
 
-    for (int i = 0; i < n; i++) {
-        temp[arr[i]] = 1;
-    }
-
-    int val = 0;
-
-    for (int i = 1; i < sizeMax; i++) {
-        if (temp[i] == 0) {
-            val = i;
-            k--;
+    while (kth > k) {
+        if (temp < arr[index]) {
+            kth++;
+            if (kth == k)
+                return kth;
+        } else {
+            index++;
         }
-        if (k == 0) {
-            break;
-        }
+        ++temp;
     }
-    return val;
+    return kth;
 }
