16069cd5ba5946c9e74babd9ad49b7ffce8b0b72
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-30 18:29:58 +0530

    Solution to Digit Sequence Mountain Peak Finder

--- Digit Sequence Mountain Peak Finder@@67de00956ac3fec9afb90e11/code.c
+++ Digit Sequence Mountain Peak Finder@@67de00956ac3fec9afb90e11/code.c
@@ -1,6 +1,20 @@
 // Your code here...
 
+int countDigits(int n)
+{
+    int digits = 1;
+    int temp = n;
+
+    while (temp > 0) {
+        temp /= 10;
+        ++digits;
+    }
+    return digits;
+}
+
 int mountainPeak(int n)
 {
+    int digits = countDigits(n);
+    printf("nbr digit = %d\n ", digits);
     return n;
 }
