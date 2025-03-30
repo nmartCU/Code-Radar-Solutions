1548dafc61497d339d97be2ccd0cd701baca1f57
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-30 20:31:47 +0530

    Solution to Digit Increment to Next Prime

--- Digit Increment to Next Prime@@67de00956ac3fec9afb90e12/code.c
+++ Digit Increment to Next Prime@@67de00956ac3fec9afb90e12/code.c
@@ -1,5 +1,7 @@
 // Your code here...
 
+#include <stdbool.h>
+
 int countDigits(int n)
 {
     int temp = n;
@@ -12,8 +14,37 @@ int countDigits(int n)
     return digits;
 }
 
+bool isPrime(int n)
+{
+    if (n < 2)
+        return false;
+    for (int i = 2; i <= n / 2; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int untilPrime(int n)
+{
+
+}
+
 int incrementToPrimeDigits(int n)
 {
-    printf("%d\n", countDigits(n));
+    int digits = countDigits(n);
+
+    int arr[n];
+
+    int temp = n;
+
+    for (int i = 0; i < digits; i++) {
+        arr[i] = temp % 10;
+        temp /= 10;
+    }
+
+    for (int i = 0; i < digits; i++) {
+        int count = untilPrime(arr[i]);
+    }
     return n;
 }
