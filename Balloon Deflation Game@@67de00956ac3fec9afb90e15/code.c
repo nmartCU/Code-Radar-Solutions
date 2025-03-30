eddc216a5106448b8d7245693401da573e8db9f2
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-30 19:40:08 +0530

    Solution to Balloon Deflation Game

---
+++ Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
@@ -0,0 +1,20 @@
+// Your code here...
+
+int findMin(int air[], int n)
+{
+    int min = 0;
+    int pos = 0;
+
+    for (int i = 0; i < n; i++)
+        if (air[i] < min)
+            min = air[i];
+    if (min != 0)
+        return min;
+    return -1;
+}
+
+void deflateBallons(int air[], int n)
+{
+    int min = findMin(air, n);
+    printf("min = %d\n", min);
+}
