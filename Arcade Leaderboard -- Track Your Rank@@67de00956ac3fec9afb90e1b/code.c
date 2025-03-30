6e2aa9caf6c8054427e6f967cf1e2f136b6053e1
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-30 16:46:35 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -54,5 +54,39 @@ void trackPlayerRanks(int ranked[MAX_RANKED], int n, int player[MAX_RANKED], int
     for (int i = 0; i < n; i++) {
         printf("%d ", ranked[i]);
     }
+    // 100 100 50 40 40 20 10
+    // 5 25 50 120
+
+    // RESULT = 6 4 2 1
+    // 6 =>
+    // car ranked[0] = 100 && ranked[1] = 100 == 1
+    //     ranked[2] = 50 == 2 / ranked[3] && ranked[4] = 40 == 3 / ranked[5] = 20 == 4 / ranked[6] = 10 == 5 et 6 == 6
+    // 4 =>
+    // car 100 100 == 1 / 50 == 2 / 40 40 == 3 / 25 == 4 / 20 == 5 ...
+
+    // parcours mon result 5 25 50 120 => 5
+
+
+    // int rank = 1
+    // parcours mon leaderBoard 100 100 50 40 40 20 10
+    //     100 (i = 0) = 1;
+    //     100 (i = 1) = 1; prochain different rank + 1
+    //     50  (i = 2) = 2; prochain different rank + 1
+    //     40  (i = 3) = 3; 
+    //     40  (i = 4) = 3; prochain different rank + 1
+    //     20  (i = 5) = 4; prochain different rank + 1
+    //     10  (i = 6) = 
+    //     tant que le prochain == current
+    //           result[i] = rank
+    int rank = 1;
+    for (int i = 0; i < n - 1; i++) {
+        if (ranked[i] != ranked[i + 1]) {
+            rank++;
+        }
+        result[i] = rank;
+    }
+    for (int i = 0; i < n; i++) {
+        printf("%d ", result[i]);
+    }
     printf("\n");
 }
