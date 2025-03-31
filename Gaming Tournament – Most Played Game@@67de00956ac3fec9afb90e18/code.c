96c6486b5f9bc904ead650d9bc0d08665e13a756
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-31 17:17:50 +0530

    Solution to Gaming Tournament – Most Played Game

--- Gaming Tournament – Most Played Game@@67de00956ac3fec9afb90e18/code.c
+++ Gaming Tournament – Most Played Game@@67de00956ac3fec9afb90e18/code.c
@@ -12,7 +12,7 @@ int mostPlayedGame(int games[], int n)
                 ++count;
             }
         }
-        printf("COunt is %d and max is %d\n", count, max);
+        printf("COunt is %d and max is %d number is %d\n", count, max, games[i]);
         if (count > max) {
             max = count;
             ids = games[i];
