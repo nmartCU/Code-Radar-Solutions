bf830bccaeaaa26b3606f14254c8a1cac6408315
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-31 15:59:11 +0530

    Solution to Game Invitation Chain – Tracking the Likes

---
+++ Game Invitation Chain – Tracking the Likes@@67de00956ac3fec9afb90e1d/code.c
@@ -0,0 +1,19 @@
+// Your code here...
+
+int totalLikes(int n)
+{
+    // 1 
+    //    => 5 people
+    // 2
+    //    => 5 / 2 => 3 people = 3 + like(0) then share each with (people * 3) + people => 3 * 3 + 3 = 12
+    // 3
+    //    => 12 / 2 => 6 people = like(3) + people(6) == 9
+    int people = 5;
+    int like = 0;
+    for (int i = 2; i <= n; i++) {
+        people /= 2;
+        like += people;
+        people = people * people + people;
+    }
+    return like;
+}
