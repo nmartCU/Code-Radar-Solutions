4e0fec2a6a962c5be28ac70500409080370b1a74
nmartCU <nils1002.cse24@chitkara.edu.in>
2025-03-31 18:45:33 +0530

    Solution to Create a Program to Store and Display Student Records

--- Create a Program to Store and Display Student Records@@6785070ed37d6039a711ebee/code.c
+++ Create a Program to Store and Display Student Records@@6785070ed37d6039a711ebee/code.c
@@ -21,31 +21,11 @@ int main(int argc, char *const argv[])
         
     if ((scanf("%d", &n)) != 1)
         return ERROR;
-    if ((students = malloc(sizeof(student_t) * (n))) == NULL)
-        return ERROR;
-    for (int i = 0; i < n; i++) {
-        char temp[MAX_LENGTH] = {0};
-        if ((scanf("%d %s %f", &students[i].roll, temp, &students[i].marks)) != 3) {
-            for (int j = 0; j < i; j++)
-                free(students[j].name);
-            free(students);
-            return ERROR;
-        }
-        if ((students[i].name = malloc(sizeof(char) * (strlen(temp) + 1))) == NULL) {
-            for (int j = 0; j < i; j++) {
-                free(students[j].name);
-            }
-            free(students);
-            return ERROR;
-        }
-        strcpy(students[i].name, temp);
-    }
-    for (int i = 0; i < n; i++) {
-        if ((printf("Roll Number: %d, Name: %s, Marks: %0.2f\n", students[i].roll, students[i].name, students[i].marks)) < 0) {
-            return ERROR;
-        }
-        free(students[i].name);
-    }
-    free(students);
+    student_t students[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d %s %f", &students[i].roll, student[i].name, &students[i].marks);
+    for (int i = 0; i < n; i++)
+        printf("Roll Number: %d, Name: %s, Marks: %0.2f\n", students[i].roll, students[i].name, students[i].marks)
     return SUCCESS;
 }
